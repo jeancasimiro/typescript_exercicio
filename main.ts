89dbabce50b7334be294d2c6b5981f6a1f1d0d55
Jean Casimiro <casimirojean2001@gmail.com>
2023-08-10 23:17:52 -0300

    adicionando o arquivo

---
+++ main.ts
@@ -0,0 +1,8 @@
+const multiplicar = (num1: number, num2: number): number => num1 * num2
+
+const saudacao = (nome: string): string => `Olá ${nome}`
+
+console.log(multiplicar(5, 3));
+console.log(saudacao('Jean'))
+
+
